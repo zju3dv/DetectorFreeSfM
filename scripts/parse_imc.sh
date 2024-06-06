d0ff18e672c7471241b552ae55d0665b4d646d67
hexingyi <hexingyi8@gmail.com>
2024-06-06 13:27:43 +0800

    Bugfix: parse imc script

--- scripts/parse_imc.sh
+++ scripts/parse_imc.sh
@@ -4,7 +4,5 @@ DATASET_NAME=phototourism
 for SCENE_NAME in british_museum florence_cathedral_side lincoln_memorial_statue milan_cathedral mount_rushmore piazza_san_marco sagrada_familia st_pauls_cathedral london_bridge
     do
         echo "Process scene: $SCENE_NAME"
-        python -u $PROJECT_DIR/tools/parse_data/parse_IMC_dataset.py \
-                --scene_name=$SCENE_NAME \
-                --split=test \
+        python -u tools/parse_data/parse_IMC_dataset.py --IMC_base_dir="SfM_datasets/IMC_2021" --scene_name=$SCENE_NAME --split=test --output_base_dir="SfM_datasets/IMC_dataset"
     done
